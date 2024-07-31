cf9a88e718c228231db23e5e92677555437782b1
AgueFinanzas <agustinaguirrea@gmail.com>
2024-07-31 14:00:59 -0300

    CalculadoraPython

---
+++ carpetasVarias/PythonPruebas/calculadora.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import *
+from math import *
+
+ventana = tk.Tk()
+ventana.title("Calculadora")
+ventana.geometry("392x600")
+
+
+
+
+
+
+
+
+
+ventana.mainloop()
