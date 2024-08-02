eb0b4b26bf074e51e2a911e1120eee75cc692346
AgueFinanzas <agustinaguirrea@gmail.com>
2024-08-01 22:10:02 -0300

    pruebasVariasCalculadorasyDemas

--- carpetasVarias/PythonPruebas/otraspruebas.py
+++ carpetasVarias/PythonPruebas/otraspruebas.py
@@ -14,6 +14,14 @@ texto=tk.Text(ventana,height=10,width=10).place(x=280,y=10)
 
 
 
+
 ventana.mainloop()
+
+
+#crear una ventana para guardar cuadros de textos
+#la ventana tendrá que aparecer vacía al principio
+#arriba a la izquierda tendrá que tener un botón que
+#una vez que lo apriete el mismo agregará un cuadro de texto arriba
+#a la izquierda también y que podrá ser cerrado
 
 
