be858c0a4703f081da1c372c5aac380d04e08d90
AgueFinanzas <agustinaguirrea@gmail.com>
2024-08-07 16:36:39 -0300

    pruebasVariasCalculadorasyDemas

--- carpetasVarias/PythonPruebas/otraspruebas.py
+++ carpetasVarias/PythonPruebas/otraspruebas.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import *
 
 ventana=tk.Tk()
-ventana.geometry("375x600")
+ventana.geometry("800x400")
 ventana.title("Anotador")
 ventana.configure(bg="lightblue")
 
@@ -11,23 +11,29 @@ ventana.configure(bg="lightblue")
 
 #texto=tk.Text(ventana,height=10,width=10).place(x=10,y=50)
 
-
 #def crear_cuadro():
- #   cuadro_texto = tk.Text(ventana, height=5, width=10, font=12)
+#   cuadro_texto = tk.Text(ventana, height=5, width=10, font=12)
 #    cuadro_texto.place(x=10, y=50)
 
-posicion_y = 40
+posicion_y = 45
+posicion_x = 10
+limite_x = 700
 def CuadroDinamico():
+    global posicion_x
     global posicion_y
     texto=tk.Text(ventana,height=5,width=10,font=12)
-    texto.place(x=10,y=posicion_y)
-    posicion_y += 80
+    texto.place(x=posicion_x , y=posicion_y)
+    posicion_x += 100
+
+    if posicion_x >= limite_x:
+        posicion_x = 10
+        posicion_y = 150
+        
 
 
 
 altoBoton=1
 anchoBoton=1
-
 boton=tk.Button(ventana,text="+",height=altoBoton,width=anchoBoton,font=12, command=CuadroDinamico).place(x=10,y=10)
 
 
